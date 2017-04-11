4ee72ee99447a607ab990cb98ead9cb9cd545c86
Naga Prasanna Bharadwaj Saripalli <bharadwaj.saripalli@appsahyd.com>
2017-04-11 16:53:29 +0530

    revision 1

---
+++ Complete-Process.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+aws cloudformation create-stack --stack-name CreateStack-CFN --template-body file:///home/ec2-user/fd-cf/fd-cf/CreateStack-CFN.json
+
+aws cloudformation list-stack-resources --stack-name CreateStack-CFN list-exports >> ResourceDetailInStack.json
+
+git add .
+
+git commit -m 'generated output json file'
+
+git push origin master
